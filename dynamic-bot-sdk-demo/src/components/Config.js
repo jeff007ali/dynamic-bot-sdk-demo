68ac6b64f076242f7a1c5e128b3698b552e27c0d
jeff007ali <jagharia@gmail.com>
2020-06-14 23:57:48 +0530

    Added CSS

--- dynamic-bot-sdk-demo/src/components/Config.js
+++ dynamic-bot-sdk-demo/src/components/Config.js
@@ -18,50 +18,52 @@ class Config extends React.Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit} className="room_name">
-          <div className="field">
-            <label className="label">Business Id</label>
-            <div className="control">
-              <input
-                className="input"
-                placeholder="Please enter business id"
-                type="text"
-                name="businessId"
-                required
-              ></input>
+        <div className="card box">
+          <form onSubmit={this.handleSubmit} className="room_name">
+            <div className="field">
+              <label className="label">Business Id</label>
+              <div className="control">
+                <input
+                  className="input"
+                  placeholder="Please enter business id"
+                  type="text"
+                  name="businessId"
+                  required
+                ></input>
+              </div>
             </div>
-          </div>
 
-          <div className="field">
-            <label className="label">Client Id</label>
-            <div className="control">
-              <input
-                className="input"
-                placeholder="Please enter client id"
-                type="text"
-                name="clientId"
-                required
-              ></input>
+            <div className="field">
+              <label className="label">Client Id</label>
+              <div className="control">
+                <input
+                  className="input"
+                  placeholder="Please enter client id"
+                  type="text"
+                  name="clientId"
+                  required
+                ></input>
+              </div>
             </div>
-          </div>
 
-          <div className="field">
-            <label className="label">Base URL</label>
-            <div className="control">
-              <input
-                className="input"
-                placeholder="Please enter base URL"
-                type="url"
-                name="baseUrl"
-                required
-              ></input>
+            <div className="field">
+              <label className="label">Base URL</label>
+              <div className="control">
+                <input
+                  className="input"
+                  placeholder="Please enter base URL"
+                  type="url"
+                  name="baseUrl"
+                  required
+                ></input>
+              </div>
             </div>
-          </div>
 
-          <div className="buttons is-right">
-            <button className="button is-primary">🥳 Party</button>
-          </div>
-        </form>
+            <div className="buttons is-right">
+              <button className="button is-primary">🥳 Party</button>
+            </div>
+          </form>
+        </div>
         <Sdk
           businessId={this.state.businessId}
           clientId={this.state.clientId}
